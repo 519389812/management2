59794c99282e419963fb7f05d3c39524221ff4fa
Frank Lau <519389812@qq.com>
2016-09-14 10:37:05 +0800

    2

--- management/settings.py
+++ management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'wkaz7zm(ja&s8*tktenv%mv*9oazppz+tl@ow7swelm%+6u$00'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
